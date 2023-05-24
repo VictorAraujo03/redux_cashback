f34e17fa7b363530ec7a5df8dd1af1a5c1735bb3
VictorAraujo03 <victoraraujo.1079@aluno.saojudas.br>
2023-05-23 21:50:05 -0300

    Apostila Redux 09 - 23-05 - Fim da apostila

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Autor: Victor Oliveira Araujo
 //essa primeira funcão é uma criadora de ação
 //ela vai criar um contrato
 const criarContrato = (nome, taxa) => {
@@ -24,3 +25,61 @@ const solicitarCashback = (nome, valor) => {
         payload: {nome, valor}
     }
 }
+
+//reducer que manipula essa fatia (slice) de estado
+//[{nome: 'Joao', valor: 100}, {nome: 'Maria', valor: 150}] 
+const historicoDePedidosDeCashback = (historicoDePedidosDeCashbackAtual = [], acao) => {
+    if(acao.type === 'SOLICITAR_CASHBACK')
+        return [
+            ...historicoDePedidosDeCashbackAtual, 
+            acao.payload
+        ]
+    return historicoDePedidosDeCashbackAtual
+}
+
+const caixa = (dinheiroEmCaixa = 0, acao) => {
+    if(acao.type === 'SOLICITAR_CASHBACK')
+        dinheiroEmCaixa -= acao.payload.valor
+    else if (acao.type === 'CRIAR_CONTRATO')
+        dinheiroEmCaixa += acao.payload.taxa
+    return dinheiroEmCaixa
+}
+
+const contratos = (listaDeContratosAtual = [], acao) => {
+    if(acao.type === 'CRIAR_CONTRATO')
+        return [...listaDeContratosAtual, acao.payload]
+    if (acao.type === 'CANCELAR_CONTRATO')
+        return listaDeContratosAtual.filter(contrato => contrato.nome !== acao.payload.nome)
+    return listaDeContratosAtual
+}
+
+const {createStore, combineReducers} = Redux
+
+const todosOsReducers = combineReducers({
+    historicoDePedidosDeCashback,
+    caixa,
+    contratos
+})
+
+const store = createStore(todosOsReducers)
+//exibindo o estado
+console.log(store.getState())
+const acaoContratoJose = criarContrato('Jose', 50)
+store.dispatch(acaoContratoJose)
+console.log(store.getState())
+const acaoContratoMaria = criarContrato('Maria', 50)
+store.dispatch(acaoContratoMaria)
+console.log(store.getState())
+
+//solicitar cashback de 10 para a Maria
+//solicitar cashback de 20 para o Jose
+//solicitar cancelamento do contrato da Maria
+const acaoCashbackMaria = solicitarCashback('Maria', 10)
+store.dispatch(acaoCashbackMaria)
+console.log(store.getState())
+const acaoCashbackJose = solicitarCashback('Jose', 20)
+store.dispatch(acaoCashbackJose)
+console.log(store.getState())
+const acaoCancelamentoMaria = cancelarContrato('Maria')
+store.dispatch(acaoCancelamentoMaria)
+console.log(store.getState())
